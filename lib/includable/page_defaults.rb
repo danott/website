340f00c6f77ee3cfcf98c1dc5de833f33e624916
Dan Ott <danott@users.noreply.github.com>
2024-01-14 16:23:38 -0500

    Get to a minimum viable style that I like

--- lib/includable/page_defaults.rb
+++ lib/includable/page_defaults.rb
@@ -6,7 +6,17 @@ module Includable
     def render
       <<~HTML
         <!--IncludeInHeader::Text
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+
+          <meta name="description" content="The personal website of Dan Ott.">
+          <meta name="author" content="Dan Ott">
+
           <link rel="stylesheet" href="/style.css">
+          <link rel="stylesheet" href="/pygments-default.css" media="(prefers-color-scheme: light)">
+          <link rel="stylesheet" href="/pygments-monokai.css" media="(prefers-color-scheme: dark)">
+
+          <link rel="alternate" type="application/rss+xml" href="/feed.xml" title="Dan Ott's RSS Feed">
+
           <script src="/script.js"></script>
         -->
       HTML
