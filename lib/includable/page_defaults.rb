b75e3dcc6892b40b73cf2bed443168168d09a204
Dan Ott <danott@users.noreply.github.com>
2024-01-15 11:54:47 -0500

    Move stylesheets and javascript into their own folders

--- lib/includable/page_defaults.rb
+++ lib/includable/page_defaults.rb
@@ -11,13 +11,13 @@ module Includable
           <meta name="description" content="The personal website of Dan Ott.">
           <meta name="author" content="Dan Ott">
 
-          <link rel="stylesheet" href="/style.css">
-          <link rel="stylesheet" href="/pygments-default.css" media="(prefers-color-scheme: light)">
-          <link rel="stylesheet" href="/pygments-monokai.css" media="(prefers-color-scheme: dark)">
+          <link rel="stylesheet" href="/stylesheets/style.css">
+          <link rel="stylesheet" href="/stylesheets/pygments-default.css" media="(prefers-color-scheme: light)">
+          <link rel="stylesheet" href="/stylesheets/pygments-monokai.css" media="(prefers-color-scheme: dark)">
 
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" title="Dan Ott's RSS Feed">
 
-          <script src="/script.js"></script>
+          <script src="/javascripts/script.js"></script>
         -->
       HTML
     end
