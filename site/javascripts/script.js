781756761a95b6d027e03f615c217066530ac99f
Dan Ott <danott@users.noreply.github.com>
2024-01-15 16:50:07 -0500

    Remove custom element JS
    If `<eval-ruby>` isn't working in the build, there's no mechanism to
    actually compose the page and load script.js...so it's clever, but not
    practically useful.

--- site/javascripts/script.js
+++ site/javascripts/script.js
@@ -1,18 +1,3 @@
 /* @format */
 
 console.log("Hello World!");
-
-class EvalRuby extends HTMLElement {
-  connectedCallback() {
-    console.error(
-      "Unexpected element added to page! This should have been removed when building the site!"
-    );
-    console.error(this);
-    this.parentElement.removeChild(this);
-  }
-}
-
-class IncludeInHeader extends EvalRuby {}
-
-customElements.define("eval-ruby", EvalRuby);
-customElements.define("include-in-header", IncludeInHeader);
